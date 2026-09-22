3cd68e66920f4b4471a436a09a06d4d093dd3eb9
Manjeeve <123073449+Homelessman8@users.noreply.github.com>
2026-09-23 07:42:11 +1000

    Spawning Obstacles
    Created a spawning script for the obstacle and also added obstacle script to add the scrolling effect from right to left

--- sk8jump/Assets/Script/Obstacle.cs
+++ sk8jump/Assets/Script/Obstacle.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float leftEdge;
+
+    private void Start()
     {
-        
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 2f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position += GameManager.Instance.gameSpeed * Time.deltaTime * Vector3.left;
+
+        if (transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
--- sk8jump/Assets/Script/Spawner.cs
+++ sk8jump/Assets/Script/Spawner.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct SpawnableObject
     {
-        
+        public GameObject prefab;
+        [Range(0f, 1f)]
+        public float spawnChance;
     }
 
-    // Update is called once per frame
-    void Update()
+    public SpawnableObject[] objects;
+    public float minSpawnRate = 1f;
+    public float maxSpawnRate = 2f;
+
+    private void OnEnable()
     {
-        
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
     }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn()
+    {
+        float spawnChance = Random.value;
+
+        foreach (SpawnableObject obj in objects)
+        {
+            if (spawnChance < obj.spawnChance)
+            {
+                GameObject obstacle = Instantiate(obj.prefab);
+                obstacle.transform.position += transform.position;
+                break;
+            }
+
+            spawnChance -= obj.spawnChance;
+        }
+
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
 }
